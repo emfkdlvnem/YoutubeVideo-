5a0eb116d2eec138c163ea233f318d9987e3c068
YoonHyeJin <emfkdlvnem@gmail.com>
2023-07-31 05:08:30 +0900

    유튜브 영상 불러오기 및 css구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
@@ -10,22 +11,24 @@ import ScrollTopButton from './components/common/ScrollTopButton';
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 function App() {
 	return (
-		<BrowserRouter>
-			<Header />
-				<Routes>
-						<Route path='/' element={<MainPage />} />
-						<Route path="/header" element={<Header />} />
-						<Route path='/footer' element={<Footer />} />
-						<Route path='/exerciseInfo' element={<ExerciseInfo />} />
-						
-						{/* 메인페이지 슬라이드에 따른 페이지 이동 */}
-						<Route path='/exerciseInfo/:slideNumber' element={<ExerciseInfo />} />
-				</Routes>
-			<Footer />
-			<ScrollTopButton />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Header />
+					<Routes>
+							<Route path='/' element={<MainPage />} />
+							<Route path="/header" element={<Header />} />
+							<Route path='/footer' element={<Footer />} />
+							<Route path='/exerciseInfo' element={<ExerciseInfo />} />
+					</Routes>
+				<Footer />
+				<ScrollTopButton />
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
+
 export default App;
